8f7aebc9aab0819545bcdd6393f6d4af9b0e49f2
Fernando Jr <jr.fernandodasilva@gmail.com>
2024-11-12 10:38:03 -0300

    chore(app): used tailwind classes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 const Home = () => {
   return (
-    <div>
-      <h1>Hello world!</h1>
+    <div className="flex w-screen items-center justify-center">
+      <h1 className="p-5 text-red-500">Hello world!</h1>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
